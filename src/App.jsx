88f4387d9d158d62a635846c4027d1a4ea921a9a
Your Name <you@example.com>
2025-08-14 23:44:34 -0400

    added trending movies feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from './components/search'
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
-import { updateSearchCount } from './appwrite';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -21,6 +21,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
 
@@ -62,12 +63,27 @@ const App = () => {
     } finally {
       setIsLoading(false);
     }
-  }
+}
   
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    }
+    catch (e) {
+      console.error(`Error fetching trending movies: ${e}`);
+      //setErrorMessage('Error fetching trending movies. Please try again later.');
+    }
+  }
+
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
 
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -80,6 +96,23 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
         </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+   
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <a href={movie.poster_url}>
+                    <img src= {movie.poster_url} alt={movie.title}/>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
 
         <section className='all-movies'>
           <h2 className="mt-[40px]">All Movies</h2>
--- src/appwrite.js
+++ src/appwrite.js
@@ -36,3 +36,21 @@ export const updateSearchCount = async (searchTerm, movie) => {
     console.error("Appwrite error:", error);
   }
 };
+
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count")
+
+    ])
+
+    return result.documents;
+  } catch (e) {
+    console.error(e)
+  }
+
+
+
+}
